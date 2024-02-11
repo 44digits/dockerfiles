6e871bbea352659b7dda212072ecbf74c5e69290
Andrew <596107+19maps@users.noreply.github.com>
2024-02-11 13:33:52 -0800

    updated

--- scanner/flask/app.py
+++ scanner/flask/app.py
@@ -5,7 +5,10 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
-    return flask.render_template('index.html')
+    return flask.render_template(
+            'index.html', 
+            filepath = '/data/bingo',
+            filelist=['a', 'b', 'c'])
 
 @app.route("/scan", methods=["POST"])
 def scan():
@@ -16,4 +19,5 @@ def scan():
 
     _cc = subprocess.run(["ls", "-l"], capture_output=True)
 
-    return _cc.stdout.decode('ascii')
+    #return _cc.stdout.decode('ascii')
+    return index()
